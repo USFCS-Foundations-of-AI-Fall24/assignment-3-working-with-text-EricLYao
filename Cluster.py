d3ebb1da10e938f0f609cf986e88339cf7ad9f02
Eric Yao <elyao@dons.usfca.edu>
2024-09-25 16:48:54 -0700

    Finished calculate_centroid
    Part of 9/26 milestone

--- Cluster.py
+++ Cluster.py
@@ -19,7 +19,25 @@ class Cluster :
 
     ## You do this.
     def calculate_centroid(self):
-        pass
+        if not self.members:
+            return None  # no members
+        
+        # defaultdict to store token sums
+        token_sums = defaultdict(lambda: 0)
+        num_docs = len(self.members)
+
+        # Sum token counts 
+        for doc in self.members:
+            for token, count in doc.tokens.items():
+                token_sums[token] += count
+
+        # avg token counts
+        averaged_tokens = {token: count / num_docs for token, count in token_sums.items()}
+
+        # new centroid Document with averaged token counts
+        self.centroid = Document(true_class='centroid')
+        for token, avg_count in averaged_tokens.items():
+            self.centroid.tokens[token] = avg_count
 
 
 # Call like so: k_means(2, ['pos','neg'], positive_docs + negative_docs)
